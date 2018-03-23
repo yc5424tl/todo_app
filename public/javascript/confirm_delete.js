d7b7f571b7efe3d0b3dd903a3f23e10b5e532676
yc5424tl <yc5424tl@go.minneapolis.edu>
2018-03-22 23:09:24 -0500

    Confirm for delete task...not behaving.

--- public/javascript/confirm_delete.js
+++ public/javascript/confirm_delete.js
@@ -1,13 +1,28 @@
 let deleteButtons = document.querySelectorAll('.delete-button');
 deleteButtons.forEach(function(button){
     button.addEventListener('click', function(ev){
-        if(!confirm('Delete Task - Are You Sure?')){
+        if(!window.confirm('Delete Task - Are You Sure?')){
             ev.preventDefault();
         }
     })
+});
+
+document.querySelectorAll('.delete-button').forEach(button => button.addEventListener.on('click', function(ev){if(!window.confirm('Delete?')){ev.preventDefault()}}))
+
+$(function() {
+    $('.delete-button').click(function(e) {
+        e.preventDefault();
+        let dialog = ('Are You Sure?').dialog({
+            buttons: {
+                "Yes": function() {alert('Yes');},
+                "No": function() {alert('No');},
+                "Cancel": function() {alert('Cancel');
+                dialog.dialog('close');
+                }
+            }
+        })
+    })
 })
-
-
 
 
 // let deleteButtons = document.querySelectorAll('.delete-button');
